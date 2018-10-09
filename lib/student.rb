27e4b5f43cf758b7f9babd91f1f2d125959a462b
Robert Snyder <r.snyder49@gmail.com>
2018-10-09 19:37:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,19 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
+  
+  attr_accessor :id, :name, :grade 
+  
+  def initialize(name, grade, id = nil)
+    @name = name 
+    @grade = grade 
+    @id = id 
+  end 
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE students 
+    SQL
+  end 
 
 end
