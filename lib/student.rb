4c48498540afb62dd5859510f8316e5c179bdf34
Robert Snyder <r.snyder49@gmail.com>
2018-10-09 20:09:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ class Student
   end
   
   def self.new_from_db(row)
-    student = self.new(name, grade)
+    student = self.new(row[1], row[2])
     student.id = row[0]
     student.name = row[1]
     student.grade = row[2]
